b260ee9312819fe932a211a576f3b387c56105f1
Sadeka <sadekaahmed53@gmail.com>
2022-11-24 20:02:59 +0600

    Authorization

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -47,8 +47,7 @@ export const login = async (req, res) => {
     if (!user) return res.status(400).json({ msg: 'User does not exist. ' });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch)
-      return res.status(400).json({ msg: 'Invalid credentials.. ' });
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials. ' });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
